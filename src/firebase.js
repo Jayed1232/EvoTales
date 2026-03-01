a83e967a1c5becd8220244a82b1a941aa7a200b2
Jayed1232 <138985150+Jayed1232@users.noreply.github.com>
2026-03-01 21:07:21 +0600

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,6 @@
 import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
 import { getAuth, signInAnonymously } from 'firebase/auth'
-import { getDatabase } from 'firebase/database'
 
 const firebaseConfig = {
   apiKey: "AIzaSyBjDvCoipumFfn8C8boQecbIxMxktLtQ1E",
@@ -10,19 +9,18 @@ const firebaseConfig = {
   storageBucket: "inkwell-dc6fe.firebasestorage.app",
   messagingSenderId: "404056573784",
   appId: "1:404056573784:web:20798e875a0597e110872d",
-  measurementId: "G-TQJNKK3K0G",
-  databaseURL: "https://inkwell-dc6fe-default-rtdb.asia-southeast1.firebasedatabase.app"
+  measurementId: "G-TQJNKK3K0G"
 }
 
-let app, db, auth, rtdb
+let app, db, auth
+
 try {
   app = initializeApp(firebaseConfig)
   db = getFirestore(app)
   auth = getAuth(app)
-  rtdb = getDatabase(app)
   signInAnonymously(auth).catch(() => {})
 } catch (e) {
   console.warn('Firebase init failed:', e)
 }
 
-export { db, auth, rtdb }
+export { db, auth }
